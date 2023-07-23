f3d08ba640534c65ef6d553e627140f18e01db53
Elif Ezgi <71594152+eezgiekinci@users.noreply.github.com>
2023-07-23 20:01:39 +0300

    Add files via upload

---
+++ Find_Absolute_Value.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+class Program
+{
+    public static int Find_Absolute_Value(int n)
+    {
+        if (n > 0)
+            return n;
+
+        else if (n < 0)
+            return -1 * n;
+
+        else
+            return 0;
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("|{0}| = {1}", n, Find_Absolute_Value(n));
+    }
+}
