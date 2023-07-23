b83b456cf0601f0ec15d289a0be317936f13f7e9
Elif Ezgi <71594152+eezgiekinci@users.noreply.github.com>
2023-07-23 20:30:20 +0300

    Add files via upload

---
+++ From_One_To_N_Odd.cs
@@ -0,0 +1,27 @@
+﻿
+// It returns the sum of odd numbers from 1 to n.
+
+using System;
+
+class From_One_To_N_Odd
+{
+    public static int Total_Sum(int n)
+    {
+        int sum = 0;
+
+        for(int i = 1; i <= n; i+=2)
+        {
+            sum = sum + i;
+        }
+        return sum;
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The sum of the odd numbers from 1 to {0} is {1}", n, Total_Sum(n));
+    }
+}
