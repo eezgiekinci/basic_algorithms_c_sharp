306a2d13eae515608cd6c2fde4be43c4dd925977
Elif Ezgi Ekinci <71594152+eezgiekinci@users.noreply.github.com>
2023-07-28 20:35:53 +0300

    Add files via upload

---
+++ Calculate_Factorial.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp6;
+
+using System;
+
+public class Mathematics
+{
+    public static int Factorial(int number)
+    {
+        if (number <= 1)
+            return 1;   
+
+        int f = 1;
+
+        for (int i = 2; i <= number; i++)
+        {
+            f *= i;
+        }
+
+        return f;
+    }
+}
+
+
+// MAIN 
+
+class Calculate_Factorial
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("{0}!: {1}", number,
+        Mathematics.Factorial(number));
+    }
+}
+    
